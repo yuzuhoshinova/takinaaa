611630025aabad48ec7769f043d301085c4ff7bc
YuzuHoshinova <takinadesuwa@gmail.com>
2023-01-01 06:41:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="https://telegra.ph/file/0c3385e451518a33a1850.jpg" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
